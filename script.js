f7837070dd1b56220a791496a018e1d712fc1a44
zhongcai jiang <jiangzhongcai123@gmail.com>
2020-06-01 17:29:45 -0400

    finish website

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("hello")
 //q1
-const butt1=document.getElementById('button1')
-const butt2=document.getElementById('button2')
-butt1.addEventListener("click",(event)=>alert("click"))
+const button1=document.getElementById('button1')
+const button2=document.getElementById('button2')
+const buttongroup=document.getElementById('buttongroup')
+button1.addEventListener("click",(event)=> buttongroup.innerText="I'm right")
+button2.addEventListener("click",(event)=>buttongroup.innerText="No,I'm right")
+//q2
+// const largeElement=document.getElementById('largeElement')
+// largeElement.addEventListener("mouseover",(event)=>alert("Hey, I told you not to hover over me!"))
+//q4
+const submit=document.getElementById('submit')
+submit.addEventListener("click",(event)=>
+{
+    if (password.value!='12345678'){
+        alert("password is incorrect");
+    }
+}
+)
+//q5
